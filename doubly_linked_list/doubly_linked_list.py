86fbd23eba2cdec6bcbe1c6d0fcc90c7397b0999
GeraldRyan <gerald.ryan40@yahoo.com>
2020-06-09 19:56:43 -0500

    got the delete working

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -118,7 +118,12 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        old_head = self.head
+        self.head.prev = node
+        self.head = self.head.prev
+        self.head.next = old_head
+        self.head.prev = None
+
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
